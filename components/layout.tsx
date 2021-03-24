cf301ef377be7eb844c554e773020a8348e51400
NaoeS <hitsolb111081@gmail.com>
2021-03-25 00:36:18 +0900

    modify: name

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 
-const name = 'hitsolb111081'
+const name = 'Hoge'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({
